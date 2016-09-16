34d9d6d1a3f458b2ffde2c208e4f7b8fd7797923
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2016-09-16 11:46:46 +0100

    Simple EmailAddressValidator implementation to only accept email addresses ending @liferay.com and @example.com

--- src/main/java/com/example/authenticator/validator/impl/EmailAddressValidatorImpl.java
+++ src/main/java/com/example/authenticator/validator/impl/EmailAddressValidatorImpl.java
@@ -1,5 +1,9 @@
 package com.example.authenticator.validator.impl;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import com.example.authenticator.validator.EmailAddressValidator;
 
 import org.osgi.service.component.annotations.Component;
@@ -11,4 +15,18 @@ import org.osgi.service.component.annotations.Component;
 	service = EmailAddressValidator.class
 )
 public class EmailAddressValidatorImpl implements EmailAddressValidator {
+
+	@Override
+	public boolean isValidEmailAddress(String emailAddress) {
+		
+		if (_validEmailDomains.contains(
+			emailAddress.substring(emailAddress.indexOf('@')))) {
+		
+			return true;
+		}
+		return false;
+	}
+
+	private Set<String> _validEmailDomains = 
+		new HashSet<String>(Arrays.asList(new String[] {"@liferay.com", "@example.com"}));
 }
